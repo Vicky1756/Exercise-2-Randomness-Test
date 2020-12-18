d2bfdbf8e76fc552aa5b26dc6ba4563f9d6bd917
Vicky1756 <auraiwan1756@gmail.com>
2020-12-18 14:12:49 +0700

    Added Readme.MD and reodered test cases

--- testcase.py
+++ testcase.py
@@ -6,25 +6,23 @@ val2 = list(set(val))
 
 
 class TestCase(unittest.TestCase):
-
-    # check if returned element are sorted in ascending order
+    # check if there are 10 returned elements
     def test_easy1(self):
-        self.assertEqual(len(val), len(val2))
-
-    # check if there are 10 returned element
-    def test_easy2(self):
         self.assertEqual(len(val), 10)
+
+    # check if all the 10 elements are distinct 
+    def test_easy2(self):
+        self.assertEqual(len(val), len(val2))
 
     # check if every element is an integer
     def test_easy3(self):
         for n in val:
             self.assertEqual(n, int(n))
-      # check if every element is within the range of 1  to 50 both inclusive
 
+    # check if every element is within the range of 1 to 50 both inclusive
     def test_easy4(self):
         for n in val:
             self.assertTrue(1 <= n <= 50)
-
 
 if __name__ == '__main__':
     unittest.main()
